05392d1ac188a0ef73285031716ba3563e0d81cd
Bibek_16 <71329649+Bibek117@users.noreply.github.com>
2021-04-30 11:36:30 +0545

    change

---
+++ Programs/1)Basic elements of c/2.c
@@ -0,0 +1,14 @@
+//display your short biodata using printf statement, (\n)-new line and (\t)-tab
+
+#include <stdio.h>
+int main(){
+	printf("Name: Bibek Angdembe \n");
+	printf("Birth Date: 2058/10/15 \n");
+	printf("Address:Ilam,Nepal \n");
+	printf("Contact:9803444444 \n");
+	printf("Profession:Student \n");
+	printf("College: NCIT \n");
+	printf("Skills:\tc,\tc++,\tJS,\tHTML,CSS,PHP");
+	return 0;
+
+	}
---
+++ Programs/1)Basic elements of c/3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("hello"); //inline commment
+    /*This is multiple line comment
+    and is often used to leave note for others developers to understand the
+    program*/
+    return 0;
+}
